07a517ba078ec5fb9a011ba8e44329d66dcfc6fb
2yeseul <20152917@sungshin.ac.kr>
2021-04-15 14:11:51 +0900

    [fix] 전화번호목록 수정

--- src/_0414/PhoneBook.java
+++ src/_0414/PhoneBook.java
@@ -1,30 +1,20 @@
 package _0414;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PhoneBook {
     public static boolean solution(String[] phoneBook) {
+        HashMap<String, String> phoneMap = new HashMap<>();
+        for(String num : phoneBook)
+            phoneMap.put(num, num);
 
-        Arrays.sort(phoneBook);
-
-        for(int i=0;i<phoneBook.length-1;i++) {
-            for(int j=i+1;j<phoneBook.length;j++) {
-                String phone1 = phoneBook[i]; String phone2 = phoneBook[j];
-                if(phone1.startsWith(phone2)) return false;
-                if(phone2.startsWith(phone1)) return false;
+        for(String number : phoneBook) {
+            for(int i=0; i<number.length();i++) {
+                if(phoneMap.containsKey(number.substring(0, i))) return false;
             }
         }
 
         return true;
     }
 
-    public static void main(String[] args) {
-        String[] p = {"97674223","119", "1195524421"};
-        boolean answer = solution(p);
-
-        System.out.println(answer);
-    }
 }
